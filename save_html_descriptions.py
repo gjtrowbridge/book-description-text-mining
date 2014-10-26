69d82ae23831dfe1c0313ed24cee3063e44f0fd8
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-10-26 00:38:49 -0400

    Add R script

--- save_html_descriptions.py
+++ save_html_descriptions.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def load_books(category):
   f = open(category + '_book_data.txt', 'r')
@@ -14,7 +15,18 @@ def save_to_html(category):
     f.close()
     i += 1
 
-save_to_html('scifi')
-save_to_html('fiction_literature')
-save_to_html('humor')
-save_to_html('thriller')
+def save_to_text(category):
+  p = re.compile('</?([a-zA-z]|"|-|=|\s|\/)*>')
+  i = 1
+  books = load_books(category)
+  for book in books:
+    f = open(category + '/file' + str(i) + '.txt', 'w')
+    desc_no_tags = p.sub('', book['raw_description'])
+    f.write(desc_no_tags.encode('ascii', 'ignore'))
+    f.close()
+    i += 1
+
+save_to_text('scifi')
+save_to_text('fiction_literature')
+save_to_text('humor')
+save_to_text('thriller')
